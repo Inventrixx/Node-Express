cea9c0c783cd0897841edabb23898bcbb22d174e
Your Name <inveniahadouken@gmail.com>
2018-12-22 12:55:24 -0300

    limpiando codigo de indexRoutes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -17,22 +17,6 @@ router.get("/estaspensandolomismoqueyobananin", (req, res) => {
   res.send("Claro que sí bananon")
 })
 
-router.get("/api/users/edit", (req, res) => {
-  //con __dirname nos devuelve la ruta en donde estamxs paradxs. Variable global inherente de node
-  //con sendFile devuelvo un archivo. Estoy armando la ruta de manera dinámica
-  res.sendFile(path.join(__dirname, "..", "", "edit.html"));
-})
-
-router.get("/api/users/test", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "", "test.html"))
-})
-
-router.get("/api/users/new", (req, res) => {
-  //path recibe como parámetros la nivelación de las carpetas. ".." -> sería un nivel superior
-  //el tercer parámetro ("") que está vacío es para que no ingrese en ninguna carpeta
-  res.sendFile(path.join(__dirname), "..", "", "new.html")
-})
-
 router.post("/api/users/new", (req, res)=> {
   let newUser = {
     name: req.body.nombre,
